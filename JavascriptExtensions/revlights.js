55642224778d64bbbcc2720cedef3ef4c109c8d9
Michael Doolittle <mgdoolittle@gmail.com>
2023-04-08 17:52:34 -0700

    in progress

--- JavascriptExtensions/revlights.js
+++ JavascriptExtensions/revlights.js
@@ -1,55 +1,39 @@
-function isPcupLight(light) {
+const perLightRpms = [
+  [500, 3900, 4800, 6400, 7300, 99999],
+  [1000, 4400, 5200, 6600, 7400, 99999],
+  [1500, 4900, 5700, 6800, 7500, 99999],
+  [3000, 5400, 6000, 7000, 7600, 99999],
+  [4500, 5900, 6400, 7200, 7700, 99999],
+  [5350, 6400, 6800, 7400, 7800, 99999],
+  [6200, 6900, 7200, 7800, 7900, 99999],
+  [7100, 7400, 7600, 7900, 7950, 99999],
+  [7800, 7900, 8100, 8120, 8070, 99999]
+];
+
+function isPcupLight(light, rpmMatrix) {
   let rpm = $prop('DataCorePlugin.GameData.Rpms');
   let gear = $prop('DataCorePlugin.GameData.Gear');
-  let gearIndex, onRpm, offRpm;
+  let onRpm, offRpm;
 
-  // array containg the rpm that the light should be turned on per gear.
-  // last row - redline rpms.
-  const perLightRpms = [
-    [500, 3900, 4800, 6400, 7300, 99999],
-    [1000, 4400, 5200, 6600, 7400, 99999],
-    [1500, 4900, 5700, 6800, 7500, 99999],
-    [3000, 5400, 6000, 7000, 7600, 99999],
-    [4500, 5900, 6400, 7200, 7700, 99999],
-    [5350, 6400, 6800, 7400, 7800, 99999],
-    [6200, 6900, 7200, 7800, 7900, 99999],
-    [7100, 7400, 7600, 7900, 7950, 99999],
-    [7800, 7900, 8100, 8120, 8070, 99999]
-  ];
+  let rpms = rpmMatrix[light];
+  let redline = rpmMatrix[rpmMatrix.length - 1]
+  let gearIndex = parseInt(gear) - 1;
 
-  switch (gear) {
-    case "R":
-    case "N":
-    case '1':
-      gearIndex = 0;
-      break;
-    case '2':
-      gearIndex = 1;
-      break;
-    case '3':
-      gearIndex = 2;
-      break;
-    case '4':
-      gearIndex = 3;
-      break;
-    case '5':
-      gearIndex = 4;
-      break;
-    case '6':
-      gearIndex = 5;
-    default:
-      return false;
+  if (isNaN(gearIndex)) {
+    gearIndex = 0;
+  } else if (gearIndex > rpmMatrix[light].length) {
+    return false;
   }
 
   // return false if light is out of bounds.
-  if (light <= 0 || light > perLightRpms.length) {
+  if (light <= 0 || light > rpmMatrix.length) {
     return false;
   } else {
-    onRpm = perLightRpms[light - 1][gearIndex];
-    offRpm = perLightRpms.at(-1)[gearIndex];
+    onRpm = rpmMatrix[light - 1][gearIndex];
+    offRpm = rpmMatrix.at(-1)[gearIndex];
   }
 
-  if (light == perLightRpms.length) {
+  if (light == rpmMatrix.length) {
     return rpm > onRpm;
   }
 
